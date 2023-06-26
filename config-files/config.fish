35093613a0d82276efa3f8ab4979473ea2a7c652
Nelson Silva <nelson@bisect.pt>
2023-06-26 11:11:51 +0100

    config-files: Added some configuration files

---
+++ config-files/config.fish
@@ -0,0 +1,4 @@
+set PATH $PATH:/home/nelson/.cargo/bin/cargo:/home/nelson/.nvm/versions/node/v14.21.1/bin:/opt/cmake/bin
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
